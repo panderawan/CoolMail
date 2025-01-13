2c504a3d8ffdb8e62a14bfcf2aab5e5372bd63b3
Ruddy Autem <ruddy.autem@gmail.com>
2025-01-13 23:00:26 +0100

    added back firebase.js, now everything is stored in env.local

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { collection, getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
+const dbEmailRef = collection(db, "emails");
+
+export { db, auth, provider, dbEmailRef };
